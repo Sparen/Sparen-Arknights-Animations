fb6acca83c88a17856eaea7b0c7af9c6edd63a0e
Andrew Fan <andrewsuicune@gmail.com>
2020-12-05 19:49:18 -0500

    Work as of Dec 5, 2020, including Chiave attack animation

---
+++ README.txt
@@ -0,0 +1,20 @@
+Part 1 - Obtaining Animations
+1. Visit https://arknights.nuke.moe
+2. Select the character and animation. Use the green background since it's easy to use for removal, or Red/Blue based on what colors are in use on the character's sprites (They don't support transparent export yet)
+3. Hit record and record the duration of the animation (two loops is best)
+4. Hit output after waiting a bit. Note that the output window doesn't refresh, so close it and reopen to see if it's done processing
+5. Download GIF
+
+Part 2 - GIF Processing
+1. Determine the start and end frames of the animation
+2. Use gifsicle (https://www.lcdf.org/gifsicle/, install w/ Homebrew) to crop to the specified frames
+   Format: gifsicle input.gif "#start-end" -o output.gif
+   Example: gifsicle Unknown.gif "#47-123" -o output-1.gif
+3. Crop the physical gif. Image is 712x512; can use LTRB of 164 112 548 496 to isolate the sprite (384x384)
+   Note: 256x256 isn't large enough to contain animations, which is why we go for 384x384
+   gifsicle --crop 164,112+384x384 output-1.gif -o output-2.gif
+
+Part 3 - GIF to PNG Spritesheet
+- I wrote a script to do this. It takes a GIF (with a background color) and a channel corresponding to the background color ([0 1 2] corresponding to [Red Green Blue]). There's also a thresholding parameter. Raise it to remove background artifacts.
+- Note that the higher the threshold, the more likely it is that false positives will be corrected. Be careful!
+  python3 colorcorrectandspritify.py output-2.gif output-f.png 1 128
---
+++ colorcorrectandspritify.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+# GIF Background to Transparency utility by Andrew Fan
+# Thrown together on Dec 5, 2020
+# Given a GIF image with n frames and a background color that is solid red, green, or blue, converts the GIF to a PNG spritesheet with transparency
+# Thresholding parameter allows a leakier mask (e.g. if threshold is 10, [3, 254, 2] would be treated the same as [0, 255, 0]. Threshold applied as a total across all three)
+# Threshold allows areas surrounding the background color tightly to be captured
+
+# Usage:
+# colorcorrectandspritify.py <input filename (GIF)> <output filename (PNG)> <channel to use, with 0 being red, 1 being green, and 2 being blue> <thresholding parameter (int, 0-255*3 scale)>
+
+import sys
+import shutil
+import numpy as np
+from PIL import Image # https://pillow.readthedocs.io/en/stable/reference/Image.html
+from PIL import GifImagePlugin # Necessary for n_frames to be recognized. https://pillow.readthedocs.io/en/stable/_modules/PIL/GifImagePlugin.html
+
+inputpath = sys.argv[1]
+outputpath = sys.argv[2]
+channel = int(sys.argv[3])
+threshold = int(sys.argv[4])
+
+# Load input
+inimg = Image.open(inputpath)
+width, height = inimg.size
+
+# Given a palette color, retrieves the RGB as an array
+def get_rgb_pixel(paletteid):
+	palette = inimg.getpalette() # Needs to be re-acquired for each frame since it's different for each frame
+	return palette[paletteid*3:paletteid*3+3]
+
+# Given an RGB value, determines if it is within a threshold of the mask
+def is_within_threshold(pixeltuple):
+	mask = [0, 0, 0]
+	mask[channel] = 255 # Mark the channel that we will make transparent
+	# Calculate absolute difference
+	absdiff = abs(pixeltuple[0] - mask[0]) + abs(pixeltuple[1] - mask[1]) + abs(pixeltuple[2] - mask[2])
+	if absdiff <= threshold:
+		return True
+	return False
+
+# True if one of the neighbors matches the mask (represented by 2), False otherwise
+# Using a 5x5 square to catch particularly nasty bits
+# Warning: height, and width are accessed as global variables. DO NOT MUTATE
+# Note: We pass a partially filled-out mask matrix. We ONLY need to check against if it has a 2 in the slot we're observing
+# This program is designed for square sprites and we do square operations so hopefully this function will work regardless
+def neighbor_matches_mask(x, y, matrix):
+	for dx in range(x - 2, x + 3):
+		if dx < 0 or dx > width:
+			pass
+		else:
+			for dy in range(y - 2, y + 3):
+				if dy < 0 or dy > height:
+					pass
+				else:
+					if matrix[dx][dy] == 2:
+						return True
+	return False
+
+# Given a boundary pixel, adjusts the value for the channel being used for transparency. 
+# Using a shitty algorithm: 
+# 1. For the channel, find the deviation between the value and the average value of the other channels
+# 2. Adjust the channel by the difference (IE set it to the average) and alpha by the difference
+# e.g. (0, 255, 0, 255) (Green) becomes (0, 0, 0, 0)
+# e.g. (0, 240, 0, 255) (Green) becomes (0, 0, 0, 15)
+# e.g. (128, 128, 128, 255) (Green) stays the same
+# e.g. (128, 192, 128, 255) (Green) becomes (128, 128, 128, 191)
+# This is OBVIOUSLY not going to restore the original alpha value, and abuses the 1px black border around sprites
+# Thin details are going to suffer greatly as far as alpha goes as well
+# Warning: inimg is accessed as a global variable. DO NOT MUTATE
+def process_boundary_pixel(outimg, x, y, destoffsetx, destoffsety):
+	currpixel = get_rgb_pixel(inimg.getpixel((x, y)))
+	avgvalue = (currpixel[0] + currpixel[1] + currpixel[2] - currpixel[channel])/2.0
+	difference = currpixel[channel] - avgvalue
+	newpixel = currpixel
+	newpixel[channel] = int(avgvalue)
+	outimg.putpixel((destoffsetx + x, destoffsety + y), (newpixel[0], newpixel[1], newpixel[2], 255 - int(difference)))
+
+# Primary logic goes here. Generates spritesheet and processes frames of input GIF
+def process_image():
+	# Determine spritesheet size
+	# ssdims specifies the max number of sprites per row/column
+	numsprites = inimg.n_frames
+	thresholdarr = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256] # If there are more than 256 frames, we've got a problem XD
+	ssdims = 16 # Force a max of 16x16
+	for i in range(len(thresholdarr)):
+		if numsprites < thresholdarr[i]:
+			ssdims = i
+			break # Found the earliest one. Done.
+
+	print ("Identified " + str(numsprites) + " frames in GIF. Generating " + str(ssdims) + "x" + str(ssdims) + " spritesheet")
+
+	outimg = Image.new("RGBA", (ssdims * width, ssdims * height), (0, 0, 0, 0)) # Initialize to transparent
+
+	# Operate over the frames of the copy. We go pixel by pixel within each frame so this can be SUPER slow
+	for i in range(inimg.n_frames):
+		print ("Processing Frame #" + str(i))
+		# Define our destination pixel offsets based off of the current frame
+		# We will add these whenever running putpixel on our destination image
+		destoffsetx = i%ssdims * width
+		destoffsety = i//ssdims * height
+
+		# Construct a 0-1-2 matrix detailing what operations to perform
+		inimg.seek(i)
+		matrix = np.zeros(inimg.size) # wxh
+		# By default, matrix has 0s. We will mark pixels with 2 if they match the mask or are within a threshold
+		for y in range(width):
+			for x in range(height):
+				if is_within_threshold(get_rgb_pixel(inimg.getpixel((x, y)))):
+					matrix[x][y] = 2
+		# Check for adjacent pixels
+		for y in range(width):
+			for x in range(height):
+				# To do this check efficiently, we pass the partially generated matrix, which has been prefilled with 2s
+				# We're mutating it live, but that doesn't matter since the neighbor_matches_mask() only checks against 2s
+				if matrix[x][y] == 0 and neighbor_matches_mask(x, y, matrix): 
+					matrix[x][y] = 1
+		# Apply changes to the OUTPUT spritesheet
+		# Note that we do not write transparent pixels because they are transparent when the output image is created
+		for y in range(width):
+			for x in range(height):
+				if matrix[x][y] == 1: # Adjust the specific channel
+					process_boundary_pixel(outimg, x, y, destoffsetx, destoffsety)
+				elif matrix[x][y] == 0: # Paste the existing data
+					currpixel = get_rgb_pixel(inimg.getpixel((x, y)))
+					outimg.putpixel((destoffsetx + x, destoffsety + y), (currpixel[0], currpixel[1], currpixel[2]))
+
+	# Push changes
+	outimg.save(outputpath)
+
+# Run the main function
+process_image()
+
